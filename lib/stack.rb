cfc6749c9eb6669509f2b721d5a335d3716a544d
Rebecca Sissman <rsissman15@aol.com>
2023-03-11 04:22:11 -0500

    commit tree b40ff40b8ea19f6b0d7f1369bc677ea7511da6d5

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
-# your code here
+class Stack
+    def initialize
+        @stack=[]
+    end
+
+    def push(num)
+        @stack.push(num)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek()
+        @stack.peek()
+    end
+
+   
+end
